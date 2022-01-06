f22802c4353e7c1e9e333b048d06370861fe7c0d
minhwan <mhko1998@gist.ac.kr>
2022-01-06 17:35:31 +0900

    modified:   test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import getnet
 import dataloader
 import training
 import torch.nn as nn
+import os.path
 
 def testing():
     device=torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
@@ -12,19 +13,27 @@ def testing():
     net.eval()
     net1=getnet.reduceNet()
     net.load_state_dict(torch.load("/home/minhwan/cifar100/modelstate/cifar-resnet34-185.pt"),strict=False)
-
+    # for key in net.state_dict():
+    #     print(key)
+    # for key in net1.state_dict():
+    #     print(key)
     for key in net.state_dict():
+        # print(key)
         if "fc" not in key:
             net1.state_dict()[key]=net.state_dict()[key]
         else:
-            for i in range(20):
-                net1.state_dict()[key][i]=net.state_dict()[key][i]
+            net1.state_dict()[key]=net.state_dict()[key][:20]
+    
+    
+    
+    # print(net1.state_dict()["network.conv1.weight"])
+    # print(net.state_dict()["network.conv1.weight"])
 
     _, testloader = dataloader.data_loader()
     net1.to(device)
     criterion = nn.CrossEntropyLoss()
 
-    training.test(net1,testloader,criterion,device)
+    # training.test(net1,testloader,criterion,device)
 
 if __name__=='__main__':
     testing()
