301a45abe10243da52247c950335160645cd19ee
minhwan <mhko1998@gist.ac.kr>
2022-01-11 15:01:30 +0900

    modified:   getnet.py 	modified:   getoptim.py 	modified:   test.py 	modified:   training.py

--- getnet.py
+++ getnet.py
@@ -18,17 +18,35 @@ class Net(nn.Module):
         return self.network(xb)
 
 class reduceNet(nn.Module):
-    def __init__(self,net):
+    def __init__(self):
         super().__init__()
-        self.network=net
-        num_ftrs=self.network.network.fc.in_features
-        self.network.network.fc=nn.Linear(num_ftrs,100)
-        
+        self.network=torchvision.models.resnet34(pretrained=False)
+        self.network.conv1 =  nn.Conv2d(3, 64, 3, 1)
+        self.network.maxpool =  nn.MaxPool2d(1,1)
+        num_ftrs=self.network.fc.in_features
+        self.network.fc=nn.Linear(num_ftrs,20)
     def forward(self,xb):
         return self.network(xb)
 
+class decreaseNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.network=torchvision.models.resnet34(pretrained=False)
+        self.network.conv1 =  nn.Conv2d(3, 64, 3, 1)
+        self.network.maxpool =  nn.MaxPool2d(1,1)
+        num_ftrs=self.network.fc.in_features
+        self.network.fc=nn.Linear(num_ftrs,80)
+        self.fc=nn.Linear(80,20)
+    def forward(self,xb):
+        x=self.network(xb)
+        output = x
+        x=self.fc(x)
+        return x
+
 if __name__=="__main__":
     model=Net()
-    # print(model)
-    model1=reduceNet(model)
+    print(model)
+    model1=reduceNet()
     print(model1)
+    model2=decreaseNet()
+    print(model2)
